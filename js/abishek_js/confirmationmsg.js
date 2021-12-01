9843371a6bc675d26750a8d205433ef89a259f0f
Randula Yashasmith Mawaththa <85297495+Randula98@users.noreply.github.com>
2021-12-01 21:52:03 +0530

    js

---
+++ js/abishek_js/confirmationmsg.js
@@ -0,0 +1,8 @@
+var loading = document.getElementsByClassName("confmsgBox2");
+var loading1 = document.getElementsByClassName("confmsgBox1");
+
+
+function loadingconf(){
+    loading[0].style.display = "none";
+    loading1[0].style.display = "block";
+}
---
+++ js/abishek_js/trendingjobs.js
@@ -0,0 +1,38 @@
+var trending_slide = document.getElementsByClassName("imagesboximg");
+
+function loadimage1(){
+    trending_slide[0].style.display = "none";
+}
+function loadimage2(){
+    trending_slide[1].style.display = "none";
+}
+function loadimage3(){
+    trending_slide[2].style.display = "none";
+}
+function loadimage4(){
+    trending_slide[3].style.display = "none";
+}
+function loadimage5(){
+    trending_slide[4].style.display = "none";
+}
+
+// function loadimage1(){
+//     trending_slide[0].style.opacity -= 0.1;
+   
+// }
+// function loadimage2(){
+//     trending_slide[1].style.opacity -= 0.1;
+    
+// }
+// function loadimage3(){
+//     trending_slide[2].style.opacity -= 0.1;
+    
+// }
+// function loadimage4(){
+//     trending_slide[3].style.opacity -= 0.1;
+
+// }
+// function loadimage5(){
+//     trending_slide[4].style.opacity -= 0.1;
+// }
+
